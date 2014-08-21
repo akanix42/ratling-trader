551676ce07de742877636fd61cb741701574aa41
Nathan Reid <nate@nateandmor.com>
2014-08-21 14:16:22 -0500

    cleanup

--- src/ratling-trader/app/game/behaviors.js
+++ src/ratling-trader/app/game/behaviors.js
@@ -1,12 +1,5 @@
 define(function (require) {
-    //    var attackEnemy = require('game/behaviors/attack-enemy'),
-    //        cloneSelf = require('game/behaviors/clone-self')
-    //
-    //        ;
     var behaviors = {};
-    //        attackEnemy: attackEnemy,
-    //      'clone-self': cloneSelf,
-    //    };
 
     return {
         get: function (name) {
