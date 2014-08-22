827e50b71e0e651f9734108d6dba8a8dd6f16c5e
Nathan Reid <nate@nateandmor.com>
2014-08-22 08:41:00 -0500

    clean up

--- src/ratling-trader/app/composition-root.js
+++ src/ratling-trader/app/composition-root.js
@@ -1,22 +1,31 @@
 define(function (require) {
         var when = require('when'),
+
             Injector = require('injector'),
-            PlayingScreenFactory = require('ui/screens/playing-screen-factory'),
-            LosingScreenFactory = require('ui/screens/losing-screen-factory'),
-            WinningScreenFactory = require('ui/screens/winning-screen-factory'),
+            DebugLogger = require('debug-logger'),
+
+            // Game
+            Game = require('game'),
+            Engine = require('game/engine'),
+
+            // Loaders
+            EntityTemplatesLoader = require('game/loaders/entity-templates-loader'),
+            behaviorsLoader = require('promise!game/loaders/behavior-modules-loader'),
+            mixinsLoader = require('promise!game/loaders/mixin-modules-loader'),
+
+            // Game Factories
             MapFactory = require('game/map/map-factory'),
             LevelFactory = require('game/levels/level-factory'),
             TileFactory = require('game/tiles/tile-factory'),
             EntityFactory = require('game/entities/entity-factory'),
-            Game = require('game'),
-            Engine = require('game/engine'),
+
+            // UI
             UI = require('ui/ui'),
             AsciiTiles = require('ui/tiles/ascii-tiles'),
-            DebugLogger = require('debug-logger'),
-            EntityTemplatesLoader = require('game/loaders/entity-templates-loader'),
-            behaviorsLoader = require('promise!game/loaders/behavior-modules-loader'),
-            mixinsLoader = require('promise!game/loaders/mixin-modules-loader'),
-            Behaviors = require('game/behaviors');
+            LosingScreenFactory = require('ui/screens/losing-screen-factory'),
+            PlayingScreenFactory = require('ui/screens/playing-screen-factory'),
+            WinningScreenFactory = require('ui/screens/winning-screen-factory')
+            ;
 
         return CompositionRoot;
 
@@ -24,19 +33,23 @@ define(function (require) {
             var self = this;
             var injector = self.injector = new Injector();
 
-            injector.register('PlayingScreenFactory', PlayingScreenFactory);
-            injector.register('LosingScreenFactory', LosingScreenFactory);
-            injector.register('WinningScreenFactory', WinningScreenFactory);
-            injector.register('AsciiTiles', AsciiTiles, true);
-            injector.register('MapFactory', MapFactory);
-            injector.register('LevelFactory', LevelFactory);
-            injector.register('TileFactory', TileFactory);
-            injector.register('EntityTemplatesLoader', EntityTemplatesLoader, true);
-            injector.register('EntityFactory', EntityFactory);
+            injector.register('Logger', DebugLogger, true);
+
             injector.register('Game', Game);
             injector.register('Engine', Engine, true);
-            injector.register('Logger', DebugLogger, true);
+            injector.register('EntityTemplatesLoader', EntityTemplatesLoader, true);
+
+            injector.register('EntityFactory', EntityFactory);
+            injector.register('LevelFactory', LevelFactory);
+            injector.register('MapFactory', MapFactory);
+            injector.register('TileFactory', TileFactory);
+
             injector.register('UI', UI);
+            injector.register('AsciiTiles', AsciiTiles, true);
+            injector.register('LosingScreenFactory', LosingScreenFactory);
+            injector.register('PlayingScreenFactory', PlayingScreenFactory);
+            injector.register('WinningScreenFactory', WinningScreenFactory);
+
             injector.register('Behaviors', function () {
                 return behaviorsLoader;
             }, true);
