aba0d866f1f84add33d2656b14dd0a7797127edc
Nathan Reid <nathan@nathantreid.com>
2015-01-01 10:46:55 -0800

    clean up

--- src/ratling-trader/app/ui/tiles/ascii-tiles.js
+++ src/ratling-trader/app/ui/tiles/ascii-tiles.js
@@ -4,7 +4,10 @@ define(function (require) {
 
     function AsciiTiles(asciiLoader) {
         var self = this;
+        var tiles = asciiLoader.getAll();
+
         self.get = get;
+
         function get(tile) {
             var entityTemplate = tile.getArchitecture().getType();
 
@@ -13,27 +16,5 @@ define(function (require) {
 
             return tiles[entityTemplate] || tiles['unknown'];
         }
-
-        var tiles = asciiLoader.getAll();
-        //defineTiles();
-        //
-        //function defineTiles() {
-        //    addTile(entityTemplatesLoader.get('null'), new AsciiTile('8', 'gray'));
-        //    addTile(entityTemplatesLoader.get('stoneFloor'), new AsciiTile('.'));
-        //    addTile(entityTemplatesLoader.get('stoneWall'), new AsciiTile('#', 'gray'));
-        //    addTile(entityTemplatesLoader.get('dirtWall'), new AsciiTile('#', 'goldenrod'));
-        //    addTile(entityTemplatesLoader.get('dirtFloor'), new AsciiTile('.', 'goldenrod'));
-        //    addTile(entityTemplatesLoader.get('player'), new AsciiTile('@', 'white'));
-        //    addTile(entityTemplatesLoader.get('fungus'), new AsciiTile('F', '#66FF00'));
-        //    addTile({name: 'unknown'}, new AsciiTile('?', 'purple'));
-        //
-        //
-        //}
-        //
-        //function addTile(entityTemplate, uiTile) {
-        //    tiles[entityTemplate.name] = uiTile;
-        //
-        //}
     }
-
 });
