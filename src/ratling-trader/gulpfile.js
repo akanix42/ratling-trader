a50b88692dfa04ef6aeed4a5c6aff1eafe2f9314
Nathan Reid <nathan@nathantreid.com>
2015-01-11 00:15:26 -0600

    ES 6 transpiler in place

--- src/ratling-trader/gulpfile.js
+++ src/ratling-trader/gulpfile.js
@@ -1,39 +1,42 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
+    changed = require('gulp-changed'),
     traceur = require('gulp-traceur'),
     to5 = require('gulp-6to5'),
     plumber = require('gulp-plumber'),
-    es6Path = 'es6/**/*.js',
     compilePath = '6to5',
     lazypipe = require('lazypipe');
 
-var jsGlobs = [
-    '**/*.js',
-    '!6to5{,/**}',
-    //es6Path,
-    '!**/bower_components{,/**}',
-    //'!es6/bower_components{,/**}'
-];
-var otherGlobs = [
-    '**',
-    '!**/*.js',
+var standardExcludes = [
     '!.idea{,/**}',
     '!6to5{,/**}',
     '!**/bower_components{,/**}',
-    '!**/node_modules{,/**}',
+    '!**/node_modules{,/**}'
+];
+var jsGlobs = ['**/*.js'].concat(standardExcludes);
+var otherGlobs = [
+    '**',
+    '!**/*.js',
+].concat(standardExcludes);
+
+var bowerGlob = [
+    '**/bower_components/**'
 ];
 var traceurTasks = lazypipe()
     .pipe(plumber)
+    .pipe(changed, compilePath + '/traceur')
     .pipe(traceur, {blockBinding: true})
     .pipe(gulp.dest, compilePath + '/traceur');
 
 var sixTo5Tasks = lazypipe()
     .pipe(plumber)
-    .pipe(to5)
+    .pipe(changed, compilePath)
+    //.pipe(to5)
     .pipe(gulp.dest, compilePath);
 
-var otherTasks = lazypipe()
+var straightCopyTasks = lazypipe()
     .pipe(plumber)
+    .pipe(changed, compilePath)
     .pipe(gulp.dest, compilePath);
 
 gulp.task('traceur', function () {
@@ -48,17 +51,34 @@ gulp.task('6to5', function () {
 
 gulp.task('other files', function () {
     return gulp.src(otherGlobs)
-        .pipe(otherTasks());
-})
-gulp.task('watch', function () {
+        .pipe(straightCopyTasks());
+});
+
+gulp.task('bower files', function () {
+    return gulp.src(bowerGlob)
+        .pipe(straightCopyTasks());
+});
+
+gulp.task('watch js', function () {
 
     return gulp.src(jsGlobs)
         .pipe(watch(jsGlobs))
         //.pipe(traceurTasks())
-        .pipe(sixTo5Tasks())
-        .pipe(otherTasks());
-
+        .pipe(sixTo5Tasks());
 });
 
-gulp.task('default', ['6to5', 'watch']);
+gulp.task('watch', ['watch other files', 'watch js', 'watch bower files']);
+
+gulp.task('watch other files', function () {
+    return gulp.src(otherGlobs)
+        .pipe(watch(otherGlobs))
+        .pipe(straightCopyTasks());
+});
+gulp.task('watch bower files', function () {
+    return gulp.src(bowerGlob)
+        .pipe(watch(bowerGlob))
+        .pipe(straightCopyTasks());
+});
+
+gulp.task('default', ['6to5', 'watch', 'other files', 'bower files']);
 
