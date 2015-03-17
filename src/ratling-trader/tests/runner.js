01b4a9fb11c4afe804fd3644034c06d4b6b2f53b
Nathan Reid <nathan@nathantreid.com>
2015-03-16 22:45:38 -0500

    Updated test runner to allow for only specific suites to be loaded

--- src/ratling-trader/tests/runner.js
+++ src/ratling-trader/tests/runner.js
@@ -3,24 +3,45 @@ define([
     // All your tests go here.
     //'spec/app.test'
     'ioc-loader',
-    'spec/mixins/cell-to-cell-movement-spec.js',
-    'spec/mixins/attacker-spec.js',
-    'spec/maps/randomly-generated-maps-spec.js',
-    'spec/game/new-game-spec.js',
-    'spec/ui/new-game-spec.js',
-    'spec/ui/loaded-game-spec',
-    'spec/ui/game-interaction-spec',
-    'promise!spec/models/tile-spec',
 ], function (iocLoader) {
     'use strict';
-    var roots = {};
-    iocLoader.init(function (gameRoot, uiRoot) {
-        roots.gameRoot = gameRoot;
-        roots.uiRoot = uiRoot;
-    }).then(function () {
-        window.console = window.console || function () {
-        };
-        window.notrack = true;
-        window.mocha.run();
+    var specs = {
+        mixins: [
+            'spec/mixins/attacker-spec.js',
+            'spec/mixins/cell-to-cell-movement-spec.js',
+        ],
+        models: [
+            'promise!spec/models/tile-spec',
+        ],
+        maps: [
+            'spec/maps/randomly-generated-maps-spec.js',
+        ],
+        game: [
+            'spec/game/new-game-spec.js',
+        ],
+        ui: [
+            'spec/ui/new-game-spec.js',
+            'spec/ui/loaded-game-spec',
+            'spec/ui/game-interaction-spec',
+        ]
+    };
+    var allTests = ['mixins', 'models', 'maps', 'game', 'ui'];
+    var qs = queryString.parse(location.search);
+    var selectedTests = qs.suite ? [qs.suite] : allTests;
+    var reqArray = [];
+    for (var i = 0; i < selectedTests.length; i++)
+        reqArray = reqArray.concat(specs[selectedTests[i]]);
+
+    require(reqArray, function () {
+        var roots = {};
+        iocLoader.init(function (gameRoot, uiRoot) {
+            roots.gameRoot = gameRoot;
+            roots.uiRoot = uiRoot;
+        }).then(function () {
+            window.console = window.console || function () {
+            };
+            window.notrack = true;
+            window.mocha.run();
+        });
     });
 });
